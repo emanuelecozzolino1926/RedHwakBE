fe3a0d36e11fbd6396ff2a446e9ab4fcb37f6cb9
emanuelecozzolino1926 <emanuele.cozzolino18@gmail.com>
2026-03-17 22:31:23 +0100

    [UPD] aggiunto CategoriaService e  IngredienteService

---
+++ src/main/java/emanueleCozzolino/redhawkbe/service/IngredienteService.java
@@ -0,0 +1,54 @@
+package emanueleCozzolino.redhawkbe.service;
+
+import emanueleCozzolino.redhawkbe.entities.Ingrediente;
+import emanueleCozzolino.redhawkbe.exceptions.BadRequestException;
+import emanueleCozzolino.redhawkbe.exceptions.NotFoundException;
+import emanueleCozzolino.redhawkbe.payload.IngredienteDTO;
+import emanueleCozzolino.redhawkbe.repositories.IngredienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class IngredienteService {
+
+    private final IngredienteRepository ingredienteRepository;
+
+    @Autowired
+    public IngredienteService(IngredienteRepository ingredienteRepository) {
+        this.ingredienteRepository = ingredienteRepository;
+    }
+
+    public List<Ingrediente> findAll() {
+        return ingredienteRepository.findAll();
+    }
+
+    public Ingrediente findById(UUID id) {
+        return ingredienteRepository.findById(id).orElseThrow(() -> new NotFoundException("L'ingrediente con id: " + id + " non è stato trovato!"));
+    }
+
+    public Ingrediente save(IngredienteDTO dto) {
+        if (ingredienteRepository.findByNome(dto.nome()).isPresent()) throw new BadRequestException("L'ingrediente " + dto.nome() + " esiste già!");
+
+        Ingrediente ingrediente = new Ingrediente();
+        ingrediente.setNome(dto.nome());
+        ingrediente.setGiacenza(dto.giacenza());
+
+        return ingredienteRepository.save(ingrediente);
+    }
+
+    public Ingrediente update(UUID id, IngredienteDTO dto) {
+        Ingrediente ingrediente = this.findById(id);
+        ingrediente.setNome(dto.nome());
+        ingrediente.setGiacenza(dto.giacenza());
+
+        return ingredienteRepository.save(ingrediente);
+    }
+
+    public void delete(UUID id) {
+        Ingrediente ingrediente = this.findById(id);
+        ingredienteRepository.delete(ingrediente);
+    }
+}
